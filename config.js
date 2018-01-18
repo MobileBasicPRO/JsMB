269f50163fcd05cde86f6129f1bb363540dad9d9
PROPHESSOR <xenteze@gmail.com>
2018-01-18 05:03:25 +0200

    Интерпретатор [Js]MobileBasic[-'a]

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const $Config = {
     type: 'graphic', //graphic/console/api
     canvas_size: ['*','*',false], //[x,y,вместить]
     Debug_Mode: true,
-    name: "Интерпретатор MobileBasic'a"
+    name: "Интерпретатор JsMobileBasic"
 }
